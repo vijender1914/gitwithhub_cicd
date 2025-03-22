5618b184f01de46ad2bf10223b576b9a8efd0953
vijender1914 <vijender1914@gmail.com>
2025-03-22 13:42:09 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket = "testing-bucketxyzz1111"
-    key    = "key/terraform.tfstate"
-    region = "ap-south-1"
+#terraform {
+ # backend "s3" {
+  #  bucket = "testing-bucketxyzz1111"
+   # key    = "key/terraform.tfstate"
+    #region = "ap-south-1"
     
-  }
-}
+ # }
+#}
